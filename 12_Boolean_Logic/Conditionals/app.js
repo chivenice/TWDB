4320c3a5e9f73f7953ffc40d93995012f62d6385
chivenice <chivenice29@gmail.com>
2021-08-25 22:25:12 +0900

    JS Conditionals

---
+++ 12_Boolean_Logic/Conditionals/app.js
@@ -0,0 +1,38 @@
+// =====================
+// BASIC IF/ELSE
+// =====================
+let random = Math.random();
+if (random < 0.5) {
+    console.log("YOUR NUMBER IS LESS THAN 0.5!!!")
+} else {
+    console.log("YOUR NUMBER IS GREATER (OR EQUAL) THAN 0.5!!!")
+}
+console.log(random);
+
+// =====================
+// PROMPT EXAMPLE
+// =====================
+
+
+
+
+
+// =====================
+// TICKET PRICE EXAMPLE
+// =====================
+
+// 0-5 - FREE
+// 5 - 10 CHILD $10 
+// 10 - 65 ADULT $20 
+// 65+ SENIOR $10
+
+
+
+// =====================
+// NESTING CONDITIONALS
+// =====================
+
+
+
+// Password must be 6+ characters
+
