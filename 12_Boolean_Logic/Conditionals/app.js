50d40448e8f9b34c06546652576d8e9c2b894801
chivenice <chivenice29@gmail.com>
2021-09-08 22:09:54 +0900

    jJS learning

--- 12_Boolean_Logic/Conditionals/app.js
+++ 12_Boolean_Logic/Conditionals/app.js
@@ -10,6 +10,17 @@ if (random < 0.5) {
 console.log(random);
 
 // =====================
+// DAYs QUOTEs
+// =====================
+const dayOfWeek ="Wednesday";
+if (dayOfWeek === "Monday") {
+    console.log("Start a new week with a smoothie glass!")
+}
+else if (dayOfWeek ==="Wednesday") {
+    console.log("Grab an Uber Snack!")
+}
+console.log(dayOfWeek);
+// =====================
 // PROMPT EXAMPLE
 // =====================
 
@@ -25,14 +36,50 @@ console.log(random);
 // 5 - 10 CHILD $10 
 // 10 - 65 ADULT $20 
 // 65+ SENIOR $10
-
-
+const age = 77;
+if ( age<5 ) {
+    console.log("You are baby. Get in for free!")
+}
+else if (age <10 ) {
+    console.log("You are child. Your ticket price is $10.")
+}
+else if (age <65 ) {
+    console.log("You are adult. Your ticket price is $20.")
+}
+else  {
+    console.log("You are senior. Your ticket price is $10.")
+}
 
 // =====================
 // NESTING CONDITIONALS
 // =====================
+const userInput = "Enter your number"
+if (userInput<100){
+    if (userInput%2 == 0) {
+        console.log("You are cat!")
+    }
+    else {
+        console.log("You are dog!")
+    }
+}
+if (userInput>=90){
+    console.log("You are human!")
+}
 
-
+const password = prompt("Please enter your password");
 
 // Password must be 6+ characters
+if (password.length>=6){
 
+// CAnnot include space
+    if (password.indexOf(' ')=== -1 ){
+    console.log("Valid Password!")
+}
+else {
+    console.log("Password cannot contain spaces!")
+}}
+    else{
+        console.log("PASSWORD is TOO SHORT! Must be 6+ characters!");
+    }
+
+// CAnnot include space
