10e52c84adac06ea4999576197767259c6f4f2e7
chivenice <chivenice29@gmail.com>
2021-09-09 21:57:39 +0900

    countdown

--- Loops/app.js
+++ Loops/app.js
@@ -1,5 +1,19 @@
+
+//even nums
 for (
-    let i=0 ; i <=20 ; i+=2
+    let i=2 ; i <=20 ; i+=2
 ){
     console.log("cat");
 }
+
+//odd nums
+for (
+    let i=3 ; i <=20 ; i+=3
+){
+    console.log("dog");
+}
+
+//count down
+for (
+    let i=100; i>=10; i-=1 )
+   {console.log(i);}
