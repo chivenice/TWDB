f4cc333b6613430aa3c3a49993ab7600dfeee6e2
chivenice <chivenice29@gmail.com>
2021-09-16 23:41:27 +0900

    Select Day

--- Functions/app.js
+++ Functions/app.js
@@ -1,4 +1,4 @@
-function greet(firstName, lastName){
+/* function greet(firstName, lastName){
     console.log(`Hey there,${firstName} ${lastName[0]}.`)
 }
 function repeat(str, numTimes){
@@ -8,3 +8,14 @@ function repeat(str, numTimes){
     }
 }
 console.log(result)
+ */
+//Week
+let days =["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]
+function returnDay(day){
+    for (let i =0 ; i<=days.length ; i++){
+        if (day<1 || day>7){
+            return null;
+        }
+        else {return days[day-1];}
+    }
+}
