f178ca0424725b009b55a7c98b162f68be419ed0
chivenice <chivenice29@gmail.com>
2021-10-08 23:12:51 +0900

    best movie

--- Reduce/app.js
+++ Reduce/app.js
@@ -19,3 +19,28 @@ const max = prices.reduce((max, price)=>{
     }
     return max
 })
+
+const movies = [
+    {
+        title: 'Amadeus',
+        score: 99
+    },
+    {
+        title: 'Stand By Me',
+        score: 85
+    },
+    {
+        title: 'Parasite',
+        score: 95
+    },
+    {
+        title: 'Alien',
+        score: 90
+    }
+]
+const highestRated = movies.reduce((bestMovie,movie)=>{
+    if(movie.score > bestMovie.score){
+        return movie;
+    }
+    return bestMovie;
+})
